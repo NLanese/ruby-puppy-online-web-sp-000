9d7d1f398cd88757d34eadb660a513662281490c
Nick Lanese <nlanese21@gmail.com>
2020-07-31 17:17:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+class Dog
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do | name |
+      puts name
+    end
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+end
